82b34cce0b5d61fcd9ec141ad706b19fd55e7b3c
tsathmika6-source <tsathmika6@gmail.com>
2026-07-09 11:11:30 -0700

    Completed model classes

--- src/main/java/com/example/malabespareparts/model/CartItem.java
+++ src/main/java/com/example/malabespareparts/model/CartItem.java
@@ -1,4 +1,38 @@
 package com.example.malabespareparts.model;
 
 public class CartItem {
+
+    private part part;
+    private int quantity;
+    private double subtotal;
+
+    public CartItem(part part, int quantity, double subtotal) {
+        this.part = part;
+        this.quantity = quantity;
+        this.subtotal = subtotal;
+    }
+
+    public part getPart() {
+        return part;
+    }
+
+    public void setPart(part part) {
+        this.part = part;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
 }
--- src/main/java/com/example/malabespareparts/model/part.java
+++ src/main/java/com/example/malabespareparts/model/part.java
@@ -1,4 +1,88 @@
 package com.example.malabespareparts.model;
 
 public class part {
+
+    private String partCode;
+    private String partName;
+    private String brand;
+    private double price;
+    private int quantity;
+    private String category;
+    private String date;
+    private String image;
+
+    public part(String partCode, String partName, String brand, double price, int quantity, String category, String date, String image) {
+        this.partCode = partCode;
+        this.partName = partName;
+        this.brand = brand;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.date = date;
+        this.image = image;
+    }
+
+    public String getPartCode() {
+        return partCode;
+    }
+
+    public void setPartCode(String partCode) {
+        this.partCode = partCode;
+    }
+
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
