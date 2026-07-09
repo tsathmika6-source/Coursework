2b0b1b51672cda0df71e0afb307b0a1ae624826c
tsathmika6-source <tsathmika6@gmail.com>
2026-07-09 12:22:42 -0700

    Implemented inventory file reading

--- src/main/java/com/example/malabespareparts/Filehandler/InventoryFileHandler.java
+++ src/main/java/com/example/malabespareparts/Filehandler/InventoryFileHandler.java
@@ -1,4 +1,34 @@
 package com.example.malabespareparts.Filehandler;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class InventoryFileHandler {
+
+    public void readInventoryFile() {
+
+        try {
+
+            BufferedReader reader = new BufferedReader(
+                    new FileReader("src/main/resources/data/inventory_legacy.txt"));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                System.out.println(line);
+
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+
+            System.out.println("File cannot be read.");
+
+        }
+
+    }
+
 }
--- src/main/java/com/example/malabespareparts/HelloApplication.java
+++ src/main/java/com/example/malabespareparts/HelloApplication.java
@@ -4,16 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.example.malabespareparts.Filehandler.InventoryFileHandler;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
+
+        // Read inventory file when the application starts
+        InventoryFileHandler inventoryFileHandler = new InventoryFileHandler();
+        inventoryFileHandler.readInventoryFile();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        stage.setTitle("Malabe Spare System");
         stage.setScene(scene);
         stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
+
+
