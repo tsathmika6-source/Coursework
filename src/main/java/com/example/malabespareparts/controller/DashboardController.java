4137819d561789d81ccc3c02c70f2440e48ce64d
tsathmika6-source <tsathmika6@gmail.com>
2026-07-25 01:52:52 -0700

    Implemented inventory file reading

--- src/main/java/com/example/malabespareparts/controller/DashboardController.java
+++ src/main/java/com/example/malabespareparts/controller/DashboardController.java
@@ -1,32 +1,51 @@
 package com.example.malabespareparts.controller;
 
+import com.example.malabespareparts.filehandler.InventoryParser;
+import com.example.malabespareparts.model.Part;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class DashboardController {
-
-    @FXML
-    private TableView<?> inventoryTable;
-
-    @FXML
-    private TableColumn<?, ?> partCodeColumn;
-
-    @FXML
-    private TableColumn<?, ?> descriptionColumn;
+public  class  DashboardController implements Initializable {
 
     @FXML
-    private TableColumn<?, ?> brandColumn;
+    private TableView<Part> inventoryTable;
 
     @FXML
-    private TableColumn<?, ?> priceColumn;
+    private TableColumn<Part, String> partCodeColumn;
 
     @FXML
-    private TableColumn<?, ?> qtyColumn;
+    private TableColumn<Part, String> descriptionColumn;
 
     @FXML
-    private TableColumn<?, ?> categoryColumn;
+    private TableColumn<Part, String> brandColumn;
 
     @FXML
-    private TableColumn<?, ?> lastUpdatedColumn;
+    private TableColumn<Part, Double> priceColumn;
+
+    @FXML
+    private TableColumn<Part, Integer> qtyColumn;
+
+    @FXML
+    private TableColumn<Part, Integer> categoryColumn;
+
+    @FXML
+    private TableColumn<Part, Integer> lastUpdatedColumn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ArrayList<Part> parts=
+                InventoryParser.loadInventory("src/main/resources/com/example/malabespareparts/Data/inventory_legacy.txt");
+
+        ObservableList<Part> list = FXCollections.observableArrayList(parts);
+
+        inventoryTable.setItems(list);
+    }
 }
